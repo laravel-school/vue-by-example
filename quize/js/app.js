d5721c5717044beed7205a2c56a563978b4edc59
tisuchi <tisuchi@gmail.com>
2020-07-09 12:49:03 +0600

    Complete lession 10

--- quize/js/app.js
+++ quize/js/app.js
@@ -1,9 +1,14 @@
 let Answer = {
   props: ["answer"],
+  methods: {
+    chosen(answer) {
+      this.$emit("answer:chosen", answer);
+    },
+  },
   template: `
         <div>
             <label :for="'answer-' + answer.id">
-                <input  type="radio" name="answer" :value="'answer-' + answer.id" />
+                <input  type="radio" name="answer" :value="'answer-' + answer.id" @click="chosen(answer)"/>
                 {{ answer.title }}
             </label>
         </div>   
@@ -15,11 +20,29 @@ let Question = {
   components: {
     answer: Answer,
   },
+  data() {
+    return {
+      showNextButton: false,
+    };
+  },
+  methods: {
+    chosen(answer) {
+      this.showNextButton = true;
+
+      this.$emit("answerGiven", answer, this.question);
+    },
+  },
   template: `
         <div>
             <h4>{{ question.title }}</h4>
             
-            <answer v-for="answer in question.answers" :answer="answer"></answer>
+            <answer 
+                v-for="answer in question.answers" 
+                :answer="answer"
+                v-on:answer:chosen="chosen"
+            ></answer>
+
+            <button v-if="showNextButton">Next</button>
         </div>
     `,
 };
@@ -32,20 +55,35 @@ let Quiz = {
     return {
       questions: [],
       currentQuestion: null,
+      correctAnswers: [],
     };
   },
   mounted() {
     axios.get("../data.json").then((response) => {
       this.questions = response.data;
       this.currentQuestion = this.questions[0];
-
-      // console.log(this.currentQuestion.answers);
     });
+  },
+  methods: {
+    answerGiven(answer, question) {
+      this.correctAnswers.push({
+        question: question,
+        answer: answer,
+      });
+
+      console.log(this.correctAnswers);
+    },
   },
   template: `
         <div>
             <h1>Question x of y</h1>
-            <question v-if="currentQuestion" :question="currentQuestion"></question>
+            <question 
+                v-if="currentQuestion" 
+                :question="currentQuestion"
+                v-on:answerGiven="answerGiven"
+            ></question>
+
+            {{ correctAnswers }}
         </div>
     `,
 };
